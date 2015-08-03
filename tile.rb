1fc7bcbc91871c492d1800a2dd8ec0d236c6a5bd
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 14:12:54 -0400

    Make tile receive board and own position when initialized, created Tile#reveal! method and Tile#neighbors method, and made attr_readers for pos and board, and changed attr_reader to attr_accessor for revealed.

--- tile.rb
+++ tile.rb
@@ -1,8 +1,34 @@
 class Tile
-  attr_reader :revealed
-  
-  def initialize(has_bomb)
+  attr_accessor :revealed
+  attr_reader :pos, :board
+
+  def initialize(board, has_bomb, pos)
     @revealed = false
     @has_bomb = has_bomb
+    @board = board
+    @pos = pos
+  end
+
+  def reveal! # should this have exclamation point?
+    self.revealed = true
+  end
+
+  def neighbors
+    neighbors = []
+    differences = [ [ 0, 1],
+                    [ 1, 1],
+                    [ 1, 0],
+                    [ 1,-1],
+                    [ 0,-1],
+                    [-1,-1],
+                    [-1, 0],
+                    [-1, 1] ]
+
+    differences.each do |difference|
+      neighbor_pos = pos[0] + difference[0], pos[1] + difference[1]
+      neighbors << board[neighbor_pos] if board.on_board?(neighbor_pos)
+    end
+
+    neighbors
   end
 end
