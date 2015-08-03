c5f291beb367e9018c9cbeb1eba7c522fd0a10ba
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 12:25:24 -0400

    Create bracket methods and finish method to populate board with tiles, some tiles randomly having bombs.

--- board.rb
+++ board.rb
@@ -1,7 +1,11 @@
+require_relative 'tile'
+
 class Board
   WIDTH = 9
   HEIGHT = 9
   NUM_BOMBS = 10
+
+attr_reader :grid
 
   def initialize
     @grid = Array.new(HEIGHT) { Array.new(WIDTH) }
@@ -9,7 +13,22 @@ class Board
 
   def populate
     bomb_positions = choose_bomb_positions
-    
+
+    (0...HEIGHT).each do |row_idx|
+      (0...WIDTH).each do |col_idx|
+        has_bomb = bomb_positions.include?([row_idx, col_idx])
+        self[row_idx, col_idx] = Tile.new(has_bomb)
+      end
+    end
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(x, y, value)
+    grid[x][y] = value
   end
 
   def choose_bomb_positions
