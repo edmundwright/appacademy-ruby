ad68b0b5e03bdc2b38ab43e76dd98b73d7755373
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 12:06:54 -0400

    Define random_pos and begin populate method

--- board.rb
+++ board.rb
@@ -1,5 +1,22 @@
 class Board
+  WIDTH = 9
+  HEIGHT = 9
+
   def initialize
-    @grid = Array.new(9) { Array.new(9) }
+    @grid = Array.new(HEIGHT) { Array.new(WIDTH) }
+  end
+
+  def populate
+    bomb_positions = []
+    until bomb_positions.size == 10
+      new_pos = random_pos
+      bomb_positions << new_pos unless bomb_positions.include?(new_pos)
+    end
+  end
+
+  def random_pos
+    x_coord = rand(HEIGHT)
+    y_coord = rand(WIDTH)
+    [x_coord, y_coord]
   end
 end
