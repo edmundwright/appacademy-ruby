c2c93cf4406c3bbd784528d004ea4e96f0523ac8
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 14:15:35 -0400

    Add attr_reader for has_bomb, and define Tile#neighbor_bomb_count method.

--- tile.rb
+++ tile.rb
@@ -1,6 +1,6 @@
 class Tile
   attr_accessor :revealed
-  attr_reader :pos, :board
+  attr_reader :pos, :board, :has_bomb
 
   def initialize(board, has_bomb, pos)
     @revealed = false
@@ -30,5 +30,15 @@ class Tile
     end
 
     neighbors
+  end
+
+  def neighbor_bomb_count
+    bomb_count = 0
+
+    neighbors.each do |neighbor|
+      bomb_count += 1 if neighbor.has_bomb
+    end
+
+    bomb_count
   end
 end
