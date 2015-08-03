7edc21ae31ccd2bdcea023fd8a2fbe92a1f2179a
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 15:10:57 -0400

    Added Game#get_pos and Game#get_move and attr_reader for board.

--- game.rb
+++ game.rb
@@ -1,8 +1,37 @@
+require_relative 'board'
+
 class Game
+
+  attr_reader :board
+
   def initialize
     @board = Board.new
     @board.populate
   end
 
-  
+  def play
+
+  end
+
+  def get_pos
+    loop do
+      puts "What position?"
+      print "> "
+      input = gets.chomp.split(",").map(&:to_i)
+      return input if board.on_board?(input)
+      puts "That's not a valid position"
+    end
+  end
+
+  def get_move
+    loop do
+      puts "Flag or Reveal? (f/r)"
+      print "> "
+      move = gets.chomp.downcase
+      return move.to_sym if ["f", "r"].include?(move)
+      puts "That's not a valid move."
+    end
+  end
+
+
 end
