5494334452972f3890b969bcc565760064b245a6
Edmund Wright <edmund.wright@gmail.com>
2015-08-03 12:11:23 -0400

    Moved choose_bomb_position logic to its own method

--- board.rb
+++ board.rb
@@ -1,17 +1,24 @@
 class Board
   WIDTH = 9
   HEIGHT = 9
+  NUM_BOMBS = 10
 
   def initialize
     @grid = Array.new(HEIGHT) { Array.new(WIDTH) }
   end
 
   def populate
+    bomb_positions = choose_bomb_positions
+    
+  end
+
+  def choose_bomb_positions
     bomb_positions = []
-    until bomb_positions.size == 10
+    until bomb_positions.size == NUM_BOMBS
       new_pos = random_pos
       bomb_positions << new_pos unless bomb_positions.include?(new_pos)
     end
+    bomb_positions
   end
 
   def random_pos
