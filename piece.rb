5cbd35dccda8a3865a4bb30f8a0526e006dbfaa4
Edmund Wright <edmund.wright@gmail.com>
2015-08-06 11:53:18 -0400

    Refactor Piece, adding encapsulation, and add #valid_move_seq?, #perform_moves!, #perform_moves, and #dup.

--- piece.rb
+++ piece.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Piece
   BLACK_PAWN_DELTAS = [[1, 1], [1, -1]]
   WHITE_PAWN_DELTAS = [[-1, 1], [-1, -1]]
@@ -6,7 +8,7 @@ class Piece
     [pos[0] + delta[0], pos[1] + delta[1]]
   end
 
-  attr_reader :color, :pos, :board
+  attr_reader :color
 
   def initialize(board, pos, color)
     @is_king = false
@@ -15,21 +17,71 @@ class Piece
     @color = color
   end
 
+  def to_s
+    is_king? ? "K" : "P"
+  end
+
+  def dup(new_board)
+    self.class.new(new_board, pos.dup, color)
+  end
+
+  def perform_moves(move_sequence)
+    if valid_move_seq?(move_sequence)
+      perform_moves!(move_sequence)
+    else
+      raise InvalidMoveError
+    end
+  end
+
+  protected
+
+  def perform_moves!(move_sequence)
+    if move_sequence.length == 1
+      move = move_sequence.first
+      perform_slide(move) || perform_jump(move) || raise(InvalidMoveError)
+    else
+      move_sequence.each do |move|
+        perform_jump(move) || raise(InvalidMoveError)
+      end
+    end
+  end
+
+  private
+
+  attr_reader :pos, :board
+
+  def is_king?
+    @is_king
+  end
+
+  def valid_move_seq?(move_sequence)
+    begin
+      dup_board = board.dup
+      dup_board[pos].perform_moves!(move_sequence)
+    rescue InvalidMoveError
+      false
+    else
+      true
+    end
+  end
+
   def perform_slide(end_pos)
     return false unless possible_slides.include?(end_pos)
     move_to(end_pos)
+    true
   end
 
   def perform_jump(end_pos)
     jump_that_ends_there = possible_jumps.select do |_, second_step|
       second_step == end_pos
     end.first
-    
+
     return false unless jump_that_ends_there
 
     first_step, second_step = jump_that_ends_there
     board.remove_piece(first_step)
     move_to(second_step)
+    true
   end
 
   def move_to(end_pos)
@@ -41,10 +93,6 @@ class Piece
 
   def maybe_promote
     transform_into_king if pos[0] == board.class::SIZE - 1 || pos[0] == 0
-  end
-
-  def is_king?
-    @is_king
   end
 
   def transform_into_king
@@ -76,8 +124,7 @@ class Piece
       WHITE_PAWN_DELTAS
     end
   end
+end
 
-  def to_s
-    is_king? ? "K" : "P"
-  end
+class InvalidMoveError < StandardError
 end
