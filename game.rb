b0b7400b4277a51fe57e6b766065bc93e146ba48
Edmund Wright <edmund.wright@gmail.com>
2015-08-06 11:55:26 -0400

    Add Game#initialize and attr_readers.

--- game.rb
+++ game.rb
@@ -1,2 +1,9 @@
 class Game
+  attr_reader :board, :current_player, :other_player
+  
+  def initialize
+    @board = Board.new
+    @current_player = HumanPlayer.new(board)
+    @other_player = HumanPlayer.new(board)
+  end
 end
