c344735657d1716263afe03ad9e6eedd74e3b19f
Edmund Wright <edmund.wright@gmail.com>
2015-08-06 16:45:31 -0400

    Continue spec.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,12 @@
 require 'rspec'
 require 'game'
+
+describe Game do
+  let(:player1) 
+  describe "#play" do
+    it "doesn't crash" do
+      game = Game.new
+      expect(game.play).to_not raise_error
+    end
+  end
+end
