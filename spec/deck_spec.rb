b512aff8e26d8b7ed1ebedfaa58f7199c01131be
Edmund Wright <edmund.wright@gmail.com>
2015-08-07 18:03:59 -0400

    Finish Deck.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,19 +5,21 @@ describe Deck do
   let(:card1) { Card.new(:hearts, :king) }
   let(:card2) { Card.new(:spades, :three) }
 
-  it "initializes an array of cards" do
-    expect(deck.cards.all? { |card| card.is_a?(Card) }).to be true
-  end
+  describe "#initialize" do
+    it "initializes an array of cards" do
+      expect(deck.cards.all? { |card| card.is_a?(Card) }).to be true
+    end
 
-  it "initializes with 52 cards" do
-    expect(deck.cards.count).to eq(52)
-  end
+    it "initializes with 52 cards" do
+      expect(deck.cards.count).to eq(52)
+    end
 
-  it "initializes with unique cards" do
-    values = deck.cards.map { |card| card.value }.uniq
-    suits = deck.cards.map { |card| card.suit }.uniq
-    expect(values.length).to eq(13)
-    expect(suits.length).to eq(4)
+    it "initializes with unique cards" do
+      values = deck.cards.map { |card| card.value }.uniq
+      suits = deck.cards.map { |card| card.suit }.uniq
+      expect(values.length).to eq(13)
+      expect(suits.length).to eq(4)
+    end
   end
 
   describe "#take" do
