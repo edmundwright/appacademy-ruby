8e3fa6bd8656ad65c655ef2d7bb0e57bda714089
Edmund Wright <edmund.wright@gmail.com>
2015-08-13 16:33:02 -0400

    Fix Response#sibling_responses and refactor.

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -1,7 +1,6 @@
 class AnswerChoice < ActiveRecord::Base
   validates :text, presence: true
   validates :question_id, presence: true
-  after_destroy :log_destroy_action
 
   belongs_to :question,
     class_name: "Question",
@@ -13,8 +12,4 @@ class AnswerChoice < ActiveRecord::Base
     class_name: "Response",
     foreign_key: :answer_choice_id,
     primary_key: :id
-
-  def log_destroy_action
-    puts "Answer choice destroyed."
-  end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,7 +3,6 @@ class Response < ActiveRecord::Base
   validates :respondent_id, presence: true
   validate :respondent_has_not_already_answered_question
   validate :respondent_is_not_author
-  after_destroy :log_destroy_action
 
   belongs_to :answer_choice,
     class_name: "AnswerChoice",
@@ -19,10 +18,6 @@ class Response < ActiveRecord::Base
     through: :answer_choice,
     source: :question
 
-  def log_destroy_action
-    puts "Response destroyed."
-  end
-
   private
 
     def respondent_has_not_already_answered_question
@@ -34,8 +29,7 @@ class Response < ActiveRecord::Base
     def respondent_is_not_author
       if Poll
         .select("polls.author_id")
-        .joins("JOIN questions ON questions.poll_id = polls.id")
-        .joins("JOIN answer_choices ON answer_choices.question_id = questions.id")
+        .joins(:questions => :answer_choices)
         .where("answer_choices.id = ?", answer_choice_id)
         .first.author_id == respondent_id
           errors[:respondent] << "is the author!"
@@ -45,12 +39,10 @@ class Response < ActiveRecord::Base
     def sibling_responses
       Response
         .select("responses.*")
-        .joins("JOIN answer_choices
-          ON answer_choices.id = responses.answer_choice_id")
-        .joins("JOIN questions ON questions.id = answer_choices.question_id")
-        .where("answer_choices.id = :answer_choice_id
+        .joins(:answer_choice => :question)
+        .where("questions.id = :question_id
                 AND (:id IS NULL OR responses.id != :id)",
-               answer_choice_id: answer_choice_id, id: id)
+               question_id: question.id, id: id)
     end
 
     def sibling_responses_nicer
