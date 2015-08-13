492febb34ac9649d0365324d6e8d5e14b090fe83
Edmund Wright <edmund.wright@gmail.com>
2015-08-13 15:20:23 -0400

    Add User#completed_polls.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,47 @@ class User < ActiveRecord::Base
     class_name: "Response",
     foreign_key: :respondent_id,
     primary_key: :id
+
+  def completed_polls
+    subquery = Response
+      .select("*")
+      .where("respondent_id = ?", id)
+
+    Poll
+      .select("polls.*")
+      .joins("JOIN questions ON questions.poll_id = polls.id")
+      .joins("JOIN answer_choices ON answer_choices.question_id = questions.id")
+      .joins("LEFT OUTER JOIN (#{subquery.to_sql}) AS user_responses
+              ON user_responses.answer_choice_id = answer_choices.id")
+      .group("polls.id")
+      .having("COUNT(user_responses.respondent_id) = COUNT(DISTINCT questions.id)")
+  end
+
+  def completed_polls_sql
+    Poll.find_by_sql([<<-SQL, id])
+      SELECT
+        polls.*
+      FROM
+        polls
+      JOIN
+        questions ON questions.poll_id = polls.id
+      JOIN
+        answer_choices ON answer_choices.question_id = questions.id
+      LEFT OUTER JOIN
+        (
+          SELECT
+            *
+          FROM
+            responses
+          WHERE
+            respondent_id = ?
+        ) AS user_responses
+      ON user_responses.answer_choice_id = answer_choices.id
+      GROUP BY
+        polls.id
+      HAVING
+        COUNT(user_responses.respondent_id) = COUNT(DISTINCT questions.id)
+    SQL
+  end
+
 end
