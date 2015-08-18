7a6f4ff8cf04e4c4f8dc2df2bb60a7d2ade502e3
Edmund Wright <edmund.wright@gmail.com>
2015-08-18 14:45:33 -0400

    Refactor.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,39 +1,39 @@
 class CatsController < ApplicationController
   def index
     @cats = Cat.all
-    render :index
+    #render :index not necessary as method X automatically renders view X
   end
 
   def show
     @cat = Cat.find(params[:id])
-    render :show
+    #render :show
   end
 
   def new
     @cat = Cat.new
-    render :new
+    #render :new
   end
 
   def edit
     @cat = Cat.find(params[:id])
-    render :edit
+    #render :edit
   end
 
   def update
     @cat = Cat.find(params[:id])
+
     if @cat.update(cat_params)
-      redirect_to cat_url(@cat)
+      redirect_to @cat # can use instead of cat_url(@cat)
     else
       render :edit
     end
-
   end
 
   def create
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to cat_url(@cat)
+      redirect_to @cat
     else
       render :new
     end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -3,6 +3,6 @@ class Cat < ActiveRecord::Base
   SEXES = ["M", "F"]
 
   validates :name, presence: true
-  validates :color, inclusion: { in: COLORS, message: "is not valid." }
-  validates :sex, inclusion: { in: SEXES, message: "is not valid." }
+  validates :color, inclusion: { in: COLORS, message: "%{value} is not valid" }
+  validates :sex, inclusion: { in: SEXES, message: "%{value} is not valid" }
 end
