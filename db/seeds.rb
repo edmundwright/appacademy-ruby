fd59b4996f7d37f2c9fc5ffbfe19e04c909fb084
Edmund Wright <edmund.wright@gmail.com>
2015-08-19 11:07:39 -0400

    Add seed data for rental request model.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,33 @@
 # run rake db:reset before this
 # Cat.destroy_all
 
-Cat.create!(:color => "orange", :name => "Hairball", :sex => "F",  :birth_date => "19990105", :description => "Stray cat.")
-Cat.create!(:color => "black", :sex => "F", :name => "Catto", :birth_date => "19990101")
-Cat.create!(:color => "orange", :sex => "M", :name => "Sennacy", :description => "AppAcademy famous cat", :birth_date => "2")
+Cat.create!(:color => "orange", :name => "Hairball", :sex => "F",
+  :birth_date => "19990105", :description => "Stray cat.")
+Cat.create!(:color => "black", :sex => "F", :name => "Catto",
+  :birth_date => "19990101")
+Cat.create!(:color => "orange", :sex => "M", :name => "Sennacy",
+  :description => "AppAcademy famous cat", :birth_date => "19850905")
+
+CatRentalRequest.create!(cat_id: 1, start_date: "19000101", end_date: "19050101")
+CatRentalRequest.create!(cat_id: 1, start_date: "19030101", end_date: "19080101")
+CatRentalRequest.create!(cat_id: 1, start_date: "19060101", end_date: "19120101")
+
+CatRentalRequest.create!(cat_id: 1, start_date: "19500101", end_date: "19550101")
+
+CatRentalRequest.create!(cat_id: 1, start_date: "19700101", end_date: "19750101")
+
+CatRentalRequest.create!(cat_id: 2, start_date: "19000101", end_date: "19050101")
+CatRentalRequest.create!(cat_id: 2, start_date: "19030101", end_date: "19080101")
+CatRentalRequest.create!(cat_id: 2, start_date: "19060101", end_date: "19120101")
+
+CatRentalRequest.create!(cat_id: 2, start_date: "19500101", end_date: "19550101")
+
+CatRentalRequest.create!(cat_id: 2, start_date: "19700101", end_date: "19750101")
+
+CatRentalRequest.create!(cat_id: 3, start_date: "19000101", end_date: "19050101")
+CatRentalRequest.create!(cat_id: 3, start_date: "19030101", end_date: "19080101")
+CatRentalRequest.create!(cat_id: 3, start_date: "19060101", end_date: "19120101")
+
+CatRentalRequest.create!(cat_id: 3, start_date: "19500101", end_date: "19550101")
+
+CatRentalRequest.create!(cat_id: 3, start_date: "19700101", end_date: "19750101")
