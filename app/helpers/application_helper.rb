3506435447908dad3ce3390b65ae746e70db4459
Edmund Wright <edmund.wright@gmail.com>
2015-08-20 12:32:40 -0400

    Add Band index action and view, and helper methods.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
              value="#{form_authenticity_token}">
     HTML
   end
+
+  def list_of_linked_albums_for_band(band)
+    album_links = band.albums.map do |album|
+      <<-HTML
+        <a href="#{album_url(album)}">#{h(album.name)}</a>
+      HTML
+    end
+    album_links.join(", ").html_safe
+  end
 end
