6ff68f5ee757b18db08cf9a0354fc7aae4f2c704
Edmund Wright <edmund.wright@gmail.com>
2015-08-20 11:39:24 -0400

    Complete User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,38 @@ class User < ActiveRecord::Base
   validates :password_digest, :session_token, presence: true
   validates :password, length: {minimum: 6, allow_nil: true}
 
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = find_by_email(email)
+    return nil unless user
+    user.password_matches?(password) ? user : nil
+  end
+
   attr_reader :password
 
   def password=(new_password)
     @password = new_password
-    password_digest = BCrypt::Password.create(new_password)
+    self.password_digest = BCrypt::Password.create(new_password)
   end
 
   def password_matches?(given_password)
     BCrypt::Password.new(password_digest).is_password?(given_password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.random_token
+    save!
+    nil
+  end
+
+  private
+
+  def self.random_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= self.class.random_token
   end
 end
