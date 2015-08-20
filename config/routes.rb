9bed5066d75ea72c9d054b7f20b1a809c7425aad
Edmund Wright <edmund.wright@gmail.com>
2015-08-20 12:17:30 -0400

    Add resources and redirect from root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: redirect('/bands')
+
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
+  resources :bands
+  resources :albums
+  resources :tracks
 end
