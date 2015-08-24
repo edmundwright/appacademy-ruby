d7503b2590cb4831f5018d293d7bc72954923b33
Edmund Wright <edmund.wright@gmail.com>
2015-08-24 17:46:14 -0400

    Really complete Comments implementation (move stuff to concern).

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,11 +1,11 @@
 class Goal < ActiveRecord::Base
+  include Commentable
+
   validates :body, :user_id, presence: true
   validates :private, inclusion: { in: [true, false] }
 
   belongs_to :user
 
-  has_many :received_comments, as: :commentable,
-    class_name: "Comment",
-    foreign_key: :commentable_id
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
+  include Commentable
+
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
   has_many :goals
 
-  has_many :received_comments, as: :commentable,
-    class_name: "Comment",
-    foreign_key: :commentable_id
+
 
   has_many :authored_comments,
     class_name: "Comment",
