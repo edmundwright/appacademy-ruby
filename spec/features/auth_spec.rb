c0a2719957050c28c94611d56f95213ee80c66b8
Edmund Wright <edmund.wright@gmail.com>
2015-08-24 11:53:05 -0400

    Refactor User Authentication

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -2,106 +2,80 @@ require 'spec_helper'
 require 'rails_helper'
 
 feature 'the signup process' do
-  before :each do
-    visit "/users/new"
-  end
-
   it "has a new user page" do
+    visit "/users/new"
     expect(page).to have_content "Sign Up"
   end
 
   feature "signing up a user" do
     it "shows username on the homepage after signup" do
-      fill_in "Username", with: "Fred"
-      fill_in "Password", with: "test_password"
-      click_button "Sign Up"
-
+      sign_up(username: "Fred", password: "test_password")
       expect(page).to have_content "Fred"
     end
 
     it "displays an error if no password" do
-      fill_in "Username", with: "Fred"
-      click_button "Sign Up"
+      sign_up(username: "Fred")
 
       expect(page).to have_content "Password is too short"
     end
 
     it "displays an error if password too short" do
-      fill_in "Password", with: "test"
-      click_button "Sign Up"
-
+      sign_up(username: "Fred", password: "test")
       expect(page).to have_content "Password is too short"
     end
 
     it "displays an error if no username" do
-      fill_in "Password", with: "test_password"
-      click_button "Sign Up"
+      sign_up(password: "test_password")
 
       expect(page).to have_content "Username can't be blank"
     end
 
     it "displays an error if username not unique" do
-      fill_in "Username", with: "Fred"
-      fill_in "Password", with: "test_password"
-      click_button "Sign Up"
-
-      visit "/users/new"
-
-      fill_in "Username", with: "Fred"
-      fill_in "Password", with: "test_password"
-      click_button "Sign Up"
+      2.times { sign_up(username: "Fred", password: "test_password") }
 
       expect(page).to have_content "Username has already been taken"
+    end
+
+    it "keeps inputted username if error" do
+      sign_up(username: "Fred")
+
+      expect(page).to have_field('Username', :with => 'Fred')
     end
   end
 end
 
 feature "logging in" do
   before :each do
-    visit "/users/new"
-
-    fill_in "Username", with: "Fred"
-    fill_in "Password", with: "test_password"
-    click_button "Sign Up"
-
-    visit "/session/new"
+    sign_up(username: "Fred", password: "test_password")
   end
 
   it "shows sign out link and username on the homepage after login" do
-    fill_in "Username", with: "Fred"
-    fill_in "Password", with: "test_password"
-    click_button "Sign In"
+    sign_in(username: "Fred", password: "test_password")
 
     expect(page).to have_content "Fred"
     expect(page).to have_content "Sign Out"
   end
 
   it "displays an error if password is wrong" do
-    fill_in "Username", with: "Fred"
-    fill_in "Password", with: "test"
-    click_button "Sign In"
+    sign_in(username: "Fred", password: "test")
 
     expect(page).to have_content "Username or password is wrong"
   end
 
   it "displays an error if no username is provided" do
-    fill_in "Password", with: "test_password"
-    click_button "Sign In"
+    sign_in(password: "test_password")
 
     expect(page).to have_content "Username or password is wrong"
   end
 
   it "displays an error if username does not exist" do
-    fill_in "Username", with: "Nancy"
-    fill_in "Password", with: "test_password"
-    click_button "Sign In"
+    sign_in(username: "Nancy", password: "test_password")
 
     expect(page).to have_content "Username or password is wrong"
   end
 end
 
 feature "logging out" do
-
   it "begins with logged out state" do
     visit "/"
 
@@ -109,10 +83,7 @@ feature "logging out" do
   end
 
   it "shows sign in link and doesn't show username on the homepage after logout" do
-    visit "/users/new"
-    fill_in "Username", with: "Fred"
-    fill_in "Password", with: "test_password"
-    click_button "Sign Up"
+    sign_up(username: "Fred", password: "test_password")
 
     click_button "Sign Out"
 
@@ -121,5 +92,4 @@ feature "logging out" do
 
     visit "/"
   end
-
 end
