f41f48c5a1236a4f5cc9e9cd651c0bf1fe5a49e2
Edmund Wright <edmund.wright@gmail.com>
2015-09-07 15:35:27 -0400

    Added Toy model, collection, views.

--- app/assets/javascripts/models/pokemon.js
+++ app/assets/javascripts/models/pokemon.js
@@ -1,3 +1,14 @@
 Pokedex.Models.Pokemon = Backbone.Model.extend({
-  urlRoot: "/pokemon"
+  urlRoot: "/pokemon",
+
+  toys: function () {
+    return this._toys = this._toys || new Pokedex.Collections.Toys();
+  },
+  parse: function(jsonResp){
+    if(jsonResp.toys){
+      this.toys().set(jsonResp.toys);
+      delete jsonResp.toys;
+    }
+    return jsonResp;
+  }
 })
--- app/assets/javascripts/views/pokemon.js
+++ app/assets/javascripts/views/pokemon.js
@@ -8,11 +8,19 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
     this.pokemon = new Pokedex.Collections.Pokemon();
     this.refreshPokemon();
 
-    this.$pokeList.on("click", "li.poke-list-item", function(event){
-      var id = $(event.currentTarget).data("id");
+    this.$pokeList.on("click", "li.poke-list-item", function(e){
+      var id = $(e.currentTarget).data("id");
       var pokemon = this.pokemon.get(id);
       this.renderPokemonDetail(pokemon);
     }.bind(this));
+
+    this.$pokeDetail.on("click", "li.toy-list-item", function(e){
+      var toyid= $(e.currentTarget).data("id");
+      var pokemonid = $(e.currentTarget).data("pokemon-id");
+      var pokemon = this.pokemon.get(pokemonid);
+      var toy = pokemon.toys().get(toyid);
+      this.renderToyDetail(toy);
+    }.bind(this))
 
     this.$newPoke.on('submit', this.submitPokemonForm.bind(this));
   },
@@ -42,8 +50,13 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
     });
 
     $div.append($ul);
-
-    this.$pokeDetail.html($div);
+    var $ultoys = $('<ul class="toys"></ul>');
+    this.$pokeDetail.html($div).append($ultoys);
+    pokemon.fetch({
+      success: function () {
+        pokemon.toys().forEach(this.addToyTolist.bind(this));
+      }.bind(this)
+    })
   },
 
   createPokemon: function(attributes, callback){
@@ -65,5 +78,31 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
     e.preventDefault();
     var formData = $(e.currentTarget).serializeJSON().pokemon;
     this.createPokemon(formData, this.renderPokemonDetail.bind(this));
+  },
+
+  addToyTolist: function (toy) {
+    $li = $('<li class="toy-list-item"></li>');
+    $li.html('Name: ' + toy.get("name") + ', Happiness: ' +
+             toy.get("happiness") + ', Price: ' + toy.get("price"));
+    $li.data("id", toy.id);
+    $li.data("pokemon-id", toy.get("pokemon_id"));
+    this.$pokeDetail.find("ul.toys").append($li);
+  },
+
+  renderToyDetail: function(toy){
+    var $div = $('<div class="detail"></div>');
+
+    var $img = $('<img src="'+ toy.get("image_url") +'">');
+    $div.append($img);
+
+    var $ul = $('<ul></ul>');
+    _.each(toy.attributes, function (attributeVal, attributeKey) {
+      $ul.append('<li>' + attributeKey + ': ' + attributeVal + '</li>');
+    });
+
+    $div.append($ul);
+
+    this.$toyDetail.html($div);
+
   }
 });
