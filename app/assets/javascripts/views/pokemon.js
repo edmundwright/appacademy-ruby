95fc732131836cd4c7c2d3af580f9c24f43e03ce
Edmund Wright <edmund.wright@gmail.com>
2015-09-07 12:21:36 -0400

    Writes Pokemon View.

--- app/assets/javascripts/views/pokemon.js
+++ app/assets/javascripts/views/pokemon.js
@@ -6,5 +6,43 @@ Pokedex.Views.Pokemon = Backbone.View.extend({
     this.$toyDetail = this.$el.find('.toy-detail');
 
     this.pokemon = new Pokedex.Collections.Pokemon();
+    this.refreshPokemon();
+
+    this.$pokeList.on("click", "li.poke-list-item", function(event){
+      var id = $(event.currentTarget).data("id");
+      var pokemon = this.pokemon.get(id);
+      this.renderPokemonDetail(pokemon);
+    }.bind(this))
+  },
+
+  addPokemonToList: function (pokemon) {
+    var $li = $('<li class="poke-list-item"></li>');
+    $li.html(pokemon.get("name") + " - " + pokemon.get("poke_type"));
+    $li.data("id", pokemon.id);
+    this.$pokeList.append($li);
+  },
+
+  refreshPokemon: function(){
+    this.pokemon.fetch({
+      success: function(){
+        this.pokemon.forEach(function(individualPokemon){
+          this.addPokemonToList(individualPokemon)
+        }.bind(this));
+      }.bind(this)
+    });
+  },
+
+  renderPokemonDetail: function(pokemon){
+    var $div = $('<div class="detail"></div>');
+    var $img = $('<img src="'+ pokemon.get("image_url") +'">');
+    $div.append($img);
+    var $ul = $('<ul></ul>');
+    _.each(pokemon.attributes, function (attributeVal, attributeKey) {
+      $ul.append('<li>' + attributeKey + ': ' + attributeVal + '</li>');
+    });
+
+    $div.append($ul);
+
+    this.$pokeDetail.html($div);
   }
 });
