fef11dd3cc25d5a3ad84c9486053c354e8511a99
Edmund Wright <edmund.wright@gmail.com>
2015-09-14 10:39:11 -0400

    Make cards and lists be rendered in the correct order

--- app/assets/javascripts/collections/lists.js
+++ app/assets/javascripts/collections/lists.js
@@ -1,5 +1,9 @@
 TrelloClone.Collections.Lists = TrelloClone.BetterCollection.extend({
   url: "api/lists",
 
+  comparator: function(list) {
+    return list.get("ord");
+  },
+
   model: TrelloClone.Models.List
 });
